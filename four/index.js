230c09233528de95889dbb6bf0f693e455d885c3
tyk2013 <ken.rad0202@gmail.com>
2016-04-22 20:41:33 +0900

    site-illustrate

---
+++ four/index.js
@@ -0,0 +1,11 @@
+//window.onload = function(){
+//    $('.top_nav').hover(
+//        function(){
+//        $('.sub_nav li').show();
+//        console.log("come");
+//    },
+//        function(){
+//        $('.sub_nav li').hide();
+//        console.log("leave");
+//    });
+//}
